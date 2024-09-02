cbdcdb6af0edafb5b9b2150991603ff6cba86483
Ind401 <115333256+Ind401@users.noreply.github.com>
2024-09-02 17:19:03 +0530

    Add files via upload

---
+++ src/components/DashboardBox.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+
+const DashboardBox = styled(Box)(({theme}) => ({
+    backgroundColor: theme.palette.background.light,
+    borderRadius: "1rem",
+    boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem black",
+    }));
+
+    export default DashboardBox;
